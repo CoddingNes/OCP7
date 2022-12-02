b630fccd92c00937948861f7210d0a41b25a4b1b
CoddingNes <axela.n@wanadoo.fr>
2022-12-02 15:58:36 +0100

    création dropdownClosed

--- src/pages/Apropos.jsx
+++ src/pages/Apropos.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import Header from '../layouts/Header';
 import Footer from '../layouts/Footer';
+import DropdownClosed from '../components/DropdownClosed';
 
 
 const Apropos = () => {
+    const dropdownTitles = ["Fiabilité", "Respect", "Service", "Responsabilité"]
     return (
         <div>
             <Header />
             <main>
-                <h1>A propos</h1>
-
+                {dropdownTitles.map((title, index) =>
+                    <ul>
+                        <DropdownClosed key={index} title={title} />
+                    </ul>
+                )}
             </main>
             <Footer />
         </div>
